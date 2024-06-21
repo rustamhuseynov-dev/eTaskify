84262c9d5e154c1fda7d5f31ae89f0853e033b78
rustemcoding571 <rustem.huseynov.2016@gmail.com>
2024-06-21 22:05:43 +0400

    first commit

---
+++ src/test/java/az/atl/project/eTaskify/ETaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atl.project.eTaskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETaskifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
